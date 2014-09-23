03ac0dff9a3a785d97547d068080265f4aab5311
zzl <zhuzhaolong0@gmail.com>
2014-09-23 11:36:59 +0800

    init

---
+++ setup.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'A scalable time series database.',
+    'author': 'Zhaolong Zhu',
+    'url': 'http://code.dapps.douban.com/Kenshin',
+    'download_url': 'http://code.dapps.douban.com/Kenshin.git',
+    'author_email': 'zhuzhaolong0@gmail.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['kenshin'],
+    'scripts': [],
+    'name': 'kenshin'
+}
+
+setup(**config)
