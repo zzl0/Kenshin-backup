13f80bac637f40b5611471e7a7138e8fac743a47
zhuzhaolong <zhuzhaolong0@gmail.com>
2015-03-06 16:48:16 +0800

    Support git versioning
    refer:
    1. http://code.dapps.douban.com/libmc/blob/master/misc/versioning.py
    2. https://github.com/jeffknupp/sandman/blob/develop/setup.py#L38

--- setup.py
+++ setup.py
@@ -5,13 +5,35 @@ try:
 except ImportError:
     from distutils.core import setup
 
+import re
+import os
+import codecs
 from glob import glob
 
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    # intentionally *not* adding an encoding option to open
+    return codecs.open(os.path.join(here, *parts), 'r').read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+long_description = read('README.md')
 
 setup(
     name='kenshin',
-    version='0.1.0',
+    version=find_version('kenshin', '__init__.py'),
     description='A scalable time series database.',
+    long_description=long_description,
     author='Zhaolong Zhu',
     url='http://code.dapps.douban.com/Kenshin',
     download_url='http://code.dapps.douban.com/Kenshin.git',
